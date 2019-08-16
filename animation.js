cf1052cbb110d505bd217b3b8fd77e34b1f29a54
Can <can.nayci@gmx.de>
2019-08-16 21:33:19 +0200

    found everything right

--- animation.js
+++ animation.js
@@ -1,10 +1,33 @@
+var screenX;
 
+async function MegaTf() {
+
+  anime({
+    loop: true,
+    targets: 'img#megaman',
+    easing: 'linear',
+    translateX: [
+      {
+        duration: screenX * 6,
+        value: screenX
+      }
+    ]
+  });  
+}
 
 window.onload = function() {
-  anime({
-    targets: 'img#megaman',
-    translateX: screen.width,
-    easing: 'linear',
-    duration: screen.width * 4
-  });
+  screenX = window.innerWidth;
+  MegaTf();
 }
+
+window.onresize = function() {
+  screenX = window.innerWidth;
+}
+
+/*
+translateX frame.width
+duration screen width * 4
+
+translateX -150
+easing: steps
+*/
